9fa1a3328b1995aa0b4479022b7ceb0a2a490284
doquockhanh <khanhquocdo.qt@gmail.com>
2020-02-28 06:36:10 +0700

    thuc hanh su dung toan tu

---
+++ Module1/#2BienKieuDuLieuToanTu/ThucHanh/SuDungToanTu/demo.js
@@ -0,0 +1,7 @@
+let inputwhidth = prompt("nhap chieu rong");
+let inputheight = prompt("nhap chieu cao");
+let whidth = parseInt(inputwhidth);
+let height = parseInt(inputheight);
+let area = whidth * height;
+document.write("dien tich la :" + area);
+
