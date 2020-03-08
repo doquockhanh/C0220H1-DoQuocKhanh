d980cf8bc1077de3c993c20ad25131305361d210
doquockhanh <khanhquocdo.qt@gmail.com>
2020-03-08 07:59:23 +0700

    bai tap luyen tap vong lap

--- Module1/#4CauTrucLap/BaiTap/#1LuyenTapSuDungVongLap/Bai8.js
+++ Module1/#4CauTrucLap/BaiTap/#1LuyenTapSuDungVongLap/Bai8.js
@@ -1,4 +1,7 @@
-function show() {
-     let a =  prompt("hãy nhập khoảng muốn đoán : từ");
-     let b = prompt("đến");
+function show(){
+     let min =  prompt("hãy nhập khoảng muốn đoán : từ");
+     let max = prompt("đến");
+     min = Math.ceil(min);
+     max = Math.floor(max);
+     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
