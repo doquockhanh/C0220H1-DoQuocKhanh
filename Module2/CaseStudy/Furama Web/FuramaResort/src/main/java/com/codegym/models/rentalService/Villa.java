09f02dac599a1e7edfbe7ad506c6c7ed4f1509e1
doquockhanh <khanhquocdo.qt@gmail.com>
2020-06-24 08:41:14 +0700

    update

--- Module2/CaseStudy/Furama Web/FuramaResort/src/main/java/com/codegym/models/rentalService/Villa.java
+++ Module2/CaseStudy/Furama Web/FuramaResort/src/main/java/com/codegym/models/rentalService/Villa.java
@@ -1,22 +1,68 @@
 package com.codegym.models.rentalService;
 
 import javax.persistence.Entity;
+import javax.persistence.Id;
 
+@Entity
 public class Villa extends FuramaService {
 
-    private Integer id;
-    private String numberOfFloor;
+    @Id
+    protected String id;
+    private Integer numberOfFloor;
+    private Integer pollArea;
+    private String roomStandard;
+    private String imageSource;
 
-    public Villa(String name, String rentPrice, String numberOfFloor) {
-        super(name, rentPrice);
-        this.numberOfFloor = numberOfFloor;
+    public Villa() {
     }
 
-    public String getNumberOfFloor() {
+    public Villa(String name, String rentPrice, Integer maxPeople, String rentType, String id,
+                 Integer numberOfFloor, Integer pollArea, String roomStandard, String imageSource) {
+        super(name, rentPrice, maxPeople, rentType);
+        this.id = id;
+        this.numberOfFloor = numberOfFloor;
+        this.pollArea = pollArea;
+        this.roomStandard = roomStandard;
+        this.imageSource = imageSource;
+    }
+
+    public Integer getNumberOfFloor() {
         return numberOfFloor;
     }
 
-    public void setNumberOfFloor(String numberOfFloor) {
+    public void setNumberOfFloor(Integer numberOfFloor) {
         this.numberOfFloor = numberOfFloor;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Integer getPollArea() {
+        return pollArea;
+    }
+
+    public void setPollArea(Integer pollArea) {
+        this.pollArea = pollArea;
+    }
+
+    public String getRoomStandard() {
+        return roomStandard;
+    }
+
+    public void setRoomStandard(String roomStandard) {
+        this.roomStandard = roomStandard;
+    }
+
+    public String getImageSource() {
+        return imageSource;
+    }
+
+    public void setImageSource(String imageSource) {
+        this.imageSource = imageSource;
     }
 }
