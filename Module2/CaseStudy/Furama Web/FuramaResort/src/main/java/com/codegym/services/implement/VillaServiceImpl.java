72fca53136e0c150932ff5d906bad556932d7fca
doquockhanh <khanhquocdo.qt@gmail.com>
2020-06-24 08:34:57 +0700

    VillaServiceImpl

---
+++ Module2/CaseStudy/Furama Web/FuramaResort/src/main/java/com/codegym/services/implement/VillaServiceImpl.java
@@ -0,0 +1,39 @@
+package com.codegym.services.implement;
+
+import com.codegym.models.rentalService.Villa;
+import com.codegym.repositories.serviceRepo.VillaRepository;
+import com.codegym.services.VillaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+@Service
+public class VillaServiceImpl implements VillaService {
+
+    @Autowired
+    VillaRepository villaRepository;
+
+    @Override
+    public Page<Villa> getAllVilla(Pageable pageable) {
+        return villaRepository.findAll(pageable);
+    }
+
+
+    //gan id cho villa moi duoc tao
+    @Override
+    public void saveVilla(Villa villa) {
+        villa.setId(geneIdForEachVilla());
+        villaRepository.save(villa);
+    }
+
+    private Integer idGene = 0;
+
+    private String geneIdForEachVilla(){
+        idGene = (int) (Math.random()*8999 + 1000);
+        if(villaRepository.existsVillaById("VL-" + (idGene))){
+            geneIdForEachVilla();
+        }
+        return "VL-" + (idGene);
+    }
+}
