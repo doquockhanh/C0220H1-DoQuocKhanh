6cd17452b741403bf8a80a662c456d9ece07b037
doquockhanh <khanhquocdo.qt@gmail.com>
2020-06-24 08:38:38 +0700

    upDate

--- Module2/CaseStudy/Furama Web/FuramaResort/src/main/java/com/codegym/controllers/MainController.java
+++ Module2/CaseStudy/Furama Web/FuramaResort/src/main/java/com/codegym/controllers/MainController.java
@@ -8,11 +8,13 @@ public class MainController {
 
     @GetMapping("/")
     public String returnHome(){
-        return "redirect:/home";
+         return "redirect:/home";
     }
 
-    @GetMapping("/home")
-    public String home(){
-        return "home";
-    }
+    // todo: commit từng file vào git
+    // đã tạo được bảng villa
+    // đã add được villa
+    // todo: thêm validate cho villa
+    // todo: hiển thị đầy đủ thuộc tính villa ở home
+
 }
