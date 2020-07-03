ecdca43c597dafe40070aadb66ed39bfaa6ad057
doquockhanh <khanhquocdo.qt@gmail.com>
2020-07-03 11:18:13 +0700

    rename from lateral

--- Module2/CaseStudy/Furama Web/FuramaResort/src/main/java/com/codegym/repositories/employeeRepo/DepartmentRepository.java
+++ Module2/CaseStudy/Furama Web/FuramaResort/src/main/java/com/codegym/repositories/employeeRepo/DepartmentRepository.java
@@ -3,5 +3,5 @@ package com.codegym.repositories.employeeRepo;
 import com.codegym.models.employee.Department;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface LateralRepository extends JpaRepository<Department, Integer> {
+public interface DepartmentRepository extends JpaRepository<Department, Integer> {
 }
