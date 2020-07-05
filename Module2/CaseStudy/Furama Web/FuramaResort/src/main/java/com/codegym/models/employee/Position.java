e5057de6e1771f2d9de3c488dd7abb1c537b6634
doquockhanh <khanhquocdo.qt@gmail.com>
2020-07-05 14:07:59 +0700

    position entity remove cascade

--- Module2/CaseStudy/Furama Web/FuramaResort/src/main/java/com/codegym/models/employee/Position.java
+++ Module2/CaseStudy/Furama Web/FuramaResort/src/main/java/com/codegym/models/employee/Position.java
@@ -11,7 +11,7 @@ public class Position {
     private Integer id;
     private String name;
 
-    @OneToMany(mappedBy="position")
+    @OneToMany(mappedBy="position", cascade = CascadeType.ALL)
     private Set<Employee> employees;
 
     public Position() {
