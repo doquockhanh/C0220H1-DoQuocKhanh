dd9d8d95ac0e959a0a37ca22ede6af12b453eb6a
doquockhanh <khanhquocdo.qt@gmail.com>
2020-07-05 14:06:45 +0700

    Employee entity validate

--- Module2/CaseStudy/Furama Web/FuramaResort/src/main/java/com/codegym/models/employee/Employee.java
+++ Module2/CaseStudy/Furama Web/FuramaResort/src/main/java/com/codegym/models/employee/Employee.java
@@ -1,9 +1,12 @@
 package com.codegym.models.employee;
 
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+
 import javax.persistence.*;
 
 @Entity
-public class Employee {
+public class Employee implements Validator {
 
     @Id
     private String id;
@@ -13,15 +16,15 @@ public class Employee {
     private String phoneNumber;
     private String email;
 
-    @ManyToOne(cascade = CascadeType.ALL)
+    @ManyToOne
     @JoinColumn(name="literacy_id", nullable=false)
     private Literacy literacy;//trinh do
 
-    @ManyToOne(cascade = CascadeType.ALL)
+    @ManyToOne
     @JoinColumn(name="position_id", nullable=false)
     private Position position;//vi tri
 
-    @ManyToOne(cascade = CascadeType.ALL)
+    @ManyToOne
     @JoinColumn(name="department_id", nullable=false)
     private Department department;//bo phan
 
@@ -40,6 +43,23 @@ public class Employee {
         this.literacy = literacy;
         this.position = position;
         this.department = department;
+    }
+
+    @Override
+    public void validate(Object target, Errors errors) {
+        Employee employee = (Employee) errors;
+        if(!employee.birthday.matches("[0-9]{2}[/|-][0-9]{2}[/|-][0-9]{4}")) {
+            errors.rejectValue("birthday", "date");
+        }
+        if(!employee.email.matches("[a-zA-Z0-9\\.\\_]{1,100}[@][a-zA-Z]{1,10}[.][a-zA-Z]{1,10}")) {
+            errors.rejectValue("email", "email");
+        }
+        if(!employee.idCard.matches("[0-9]{8}")) {
+            errors.rejectValue("idCard", "id");
+        }
+        if(!employee.phoneNumber.matches("[0-9]{10}")) {
+            errors.rejectValue("phoneNumber", "phoneNumber");
+        }
     }
 
     public String getId() {
@@ -113,4 +133,10 @@ public class Employee {
     public void setDepartment(Department department) {
         this.department = department;
     }
+
+    @Override
+    public boolean supports(Class<?> clazz) {
+        return false;
+    }
+
 }
