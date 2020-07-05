b15337d0e4e3ec4ca9141fabf3c4fa4945b33757
doquockhanh <khanhquocdo.qt@gmail.com>
2020-07-05 13:54:48 +0700

    ContracDetailController crud

--- Module2/CaseStudy/Furama Web/FuramaResort/src/main/java/com/codegym/controllers/contract/ContractDetailController.java
+++ Module2/CaseStudy/Furama Web/FuramaResort/src/main/java/com/codegym/controllers/contract/ContractDetailController.java
@@ -1,7 +1,9 @@
 package com.codegym.controllers.contract;
 
 import com.codegym.models.contract.ContractDetail;
+import com.codegym.models.rentalService.AccompaniedService;
 import com.codegym.services.contract.ContractDetailService;
+import com.codegym.services.furamaService.AccompaniedService_Service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -18,25 +20,30 @@ public class ContractDetailController {
     @Autowired
     ContractDetailService contractDetailService;
 
+    @Autowired
+    AccompaniedService_Service accompaniedService_service;
+
     @GetMapping("/addContractDetail")
     public String addContractDetail(Model model){
         model.addAttribute("contractDetail", new ContractDetail());
-        return "";
+        model.addAttribute("accompaniedServices", contractDetailService.getAllContractDetail());
+        return "contract/contractDetail/addContractDetail";
     }
 
     @PostMapping("/addingContractDetail")
     public String addingContractDetail(@ModelAttribute ContractDetail contractDetail, Model model){
         contractDetailService.saveContractDetail(contractDetail);
         model.addAttribute("contractDetail", new ContractDetail());
+        model.addAttribute("accompaniedServices", contractDetailService.getAllContractDetail());
         model.addAttribute("message", "add successful !");
-        return "";
+        return "contract/contractDetail/addContractDetail";
     }
 
     @GetMapping("/getAllContractDetail")
     public String getAllContractDetail(Model model){
         List<ContractDetail> contractDetailList = contractDetailService.getAllContractDetail();
         model.addAttribute("contractDetails", contractDetailList);
-        return "";
+        return "contract/contractDetail/allContractDetail";
     }
 
     @GetMapping("/getContractDetail")
@@ -49,6 +56,22 @@ public class ContractDetailController {
     @GetMapping("/deleteContractDetail")
     public String deleteContractDetail(@RequestParam Integer id){
         contractDetailService.deleteContractDetailById(id);
-        return "";
+        return "redirect:/getAllContractDetail";
+    }
+
+    @GetMapping("/editContractDetail")
+    public String editContractDetail(@RequestParam Integer id, Model model){
+        ContractDetail contractDetail = contractDetailService.getContractDetailById(id);
+        model.addAttribute("contractDetail", contractDetail);
+        model.addAttribute("accompaniedServices", contractDetailService.getAllContractDetail());
+        return "contract/contractDetail/editContractDetail";
+    }
+
+    @PostMapping("/editingContractDetail")
+    public String editingContractDetail(@ModelAttribute ContractDetail contractDetail, Model model){
+        contractDetailService.saveContractDetail(contractDetail);
+        model.addAttribute("contractDetail", new ContractDetail());
+        model.addAttribute("message", "saved your change!");
+        return "redirect:/getAllContractDetail";
     }
 }
